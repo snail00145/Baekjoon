76c310b19b33e5d310b7d00375672ee889c4c4c4
Ian <81557317+snail00145@users.noreply.github.com>
2024-02-14 14:39:16 +0900

    [Bronze IV] Title: 숫자의 합, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,20 +1,18 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-//자릿값 반환 방법을 어떻게 짜야할지 몰라서 split사용
-public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int count = Integer.parseInt(br.readLine());
-		String str = br.readLine();
-		String[] str2= str.split("|");
-		int sum = 0;
-		
-		for (int i = 0; i < count; i++) {
-			sum += Integer.parseInt(str2[i]);
-		}
-		System.out.println(sum);
-	}
-	
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        String str = br.readLine();
+        
+        for (byte B : str.getBytes()){
+            sum += B - '0'; 
+        }
+        System.out.println(sum);
+        
+    }
 }
